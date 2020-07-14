2f26ca0567231cf9d835c9ffb4eaf67709802f6d
Ricko Hamonangan Panggabean <rickohp85@gmail.com>
2020-07-14 11:57:49 +0700

    masih bersambung, belum selesai

---
+++ main.py
@@ -0,0 +1,23 @@
+# KONSTRUKSI DASAR PYTHON
+# SEQUENTIAL > EKSEKUSI BERURUTAN
+print('Halo Dunia')
+print('by Iko')
+print('Tanggal 14 Juli 2020')
+print('=' *20)
+
+
+# PERCABANGAN > EKSEKUSI TERPILIH
+ingin_cepat = False
+if ingin_cepat:
+    print('Jalan Lurus Aja')
+else:
+    print('Muter,Lewat Jalan Lain Ya...')
+
+
+# PERULANGAN
+jumlah_anak = 8
+
+for index_anak in range(1, jumlah_anak+1):
+    print(f'Hai anak ke-{index_anak}')
+
+
